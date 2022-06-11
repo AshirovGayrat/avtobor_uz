a7a43be815d9a8bca87bd6c53599c53b06f911f7
gayrat <ashirov.dev@gmail.com>
2022-06-11 14:43:03 +0500

    I`m updated AttachController

--- src/main/java/com/company/controller/AttachController.java
+++ src/main/java/com/company/controller/AttachController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,20 +25,22 @@ public class AttachController {
     private final AttachService attachService;
 
     @ApiOperation(value = "upload Attach", notes = "Method used for upload Attach")
-    @PostMapping("/public")
-    public ResponseEntity<AttachResponseDTO> create(@RequestParam("file") MultipartFile file,
-                                                    HttpServletRequest request) {
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @PostMapping("")
+    public ResponseEntity<AttachResponseDTO> upload(@RequestParam("file") MultipartFile file) {
         log.warn("Attach upload {}{}", file.getOriginalFilename(), AttachController.class);
         return ResponseEntity.ok(attachService.upload(file));
     }
 
     @ApiOperation(value = "download", notes = "Method for download attach", nickname = "Mazgi")
-    @GetMapping("/public/download/{fileName}")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @GetMapping("/download/{fileName}")
     public ResponseEntity<Resource> download(@PathVariable("fileName") String fileName) {
         return attachService.download(fileName);
     }
 
     @ApiOperation(value = "Open general", notes = "Method used for Open general by id")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping(value = "/open-general/{id}", produces = MediaType.ALL_VALUE)
     public byte[] open_general(@PathVariable("id") String key) {
         log.info("Attach open_general {}{}", key, AttachController.class);
@@ -45,9 +48,8 @@ public class AttachController {
     }
 
     @ApiOperation(value = "delete by key", notes = "Method used for delete attach by key")
-    @DeleteMapping("/delete/{key}")
-    public ResponseEntity<Boolean> delete(@PathVariable("key") String key,
-                                          HttpServletRequest request) {
+    @DeleteMapping("/{key}")
+    public ResponseEntity<Boolean> delete(@PathVariable("key") String key) {
         log.warn("Attach delete {}{}", key, AttachController.class);
         return ResponseEntity.ok(attachService.delete(key));
     }
